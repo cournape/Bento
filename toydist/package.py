ae17330927641dd4d5e1bd326bfa5ffcd1941ede
David Cournapeau <cournape@gmail.com>
2009-10-21 11:43:21 +0900

    REF: use cabal_parser instead of pyparsing-based parser for parse_static.

--- toydist/package.py
+++ toydist/package.py
@@ -1,8 +1,8 @@
 from toydist.misc import \
         Extension
 
-from toydist.config_parser.parser import \
-        AST
+from toydist.cabal_parser.cabal_parser import \
+        parse
 
 class PackageDescription:
     def __init__(self, name, version=None, summary=None, url=None,
@@ -70,19 +70,46 @@ class PackageDescription:
 
         return d
 
+def _parse_library(parsed_dict, package_dict):
+    package_dict['extensions'] = []
+    package_dict['py_modules'] = []
+    package_dict['packages'] = []
+
+    for libname, lib in parsed_dict.items():
+        if lib.has_key('extension'):
+            for ext_name, ext_package_dict in lib['extension'].items():
+                src =  ext_package_dict['sources']
+                ext = Extension(ext_name, src)
+                package_dict['extensions'].append(ext)
+        if lib.has_key('modules'):
+            package_dict['modules'].extend(lib['modules'])
+        if lib.has_key('packages'):
+            package_dict['packages'].extend(lib['packages'])
 
 def _parse_static(cnt):
     """Parse a static file. cnt is assumed to be the content of the static file
-    in one string"""
-    ast = AST()
-    ast.parse_string(cnt)
-    return PackageDescription(**ast.to_dict())
+    in a list of strings"""
+    data = {}
+    res = parse(cnt)
+
+    # Get metadata
+    for k in ['name', 'author', 'version']:
+        try:
+            val = res[k]
+            data[k] = val
+            del res[k]
+        except KeyError:
+            pass
+
+    # Get library section
+    if res.has_key('library'):
+        _parse_library(res['library'], data)
+    return PackageDescription(**data)
 
 def parse_static(filename):
     f = open(filename)
     try:
-        cnt = "\n".join(f.readlines())
-        return _parse_static(cnt)
+        return _parse_static(f.readlines())
     finally:
         f.close()
 
