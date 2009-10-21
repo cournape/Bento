6ddf38bbe97740e4734ff33c32757b4f7354a1ee
David Cournapeau <cournape@gmail.com>
2009-10-21 16:29:57 +0900

    ENH: handle a few more metadata in info -> setup conversion.

--- toydist/conv.py
+++ toydist/conv.py
@@ -12,6 +12,10 @@ def distutils_to_package_description(dist):
     data['maintainer_email'] = dist.get_contact_email()
     data['summary'] = dist.get_description()
     data['description'] = dist.get_long_description()
+    data['license'] = dist.get_license()
+    data['platforms'] = dist.get_platforms()
+
+    data['install_requires'] = dist.install_requires
 
     data['py_modules'] = dist.py_modules
     data['packages'] = dist.packages
