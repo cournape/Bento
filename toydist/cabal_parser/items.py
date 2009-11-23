7e062b64b8d362b9338c13377da4eca6e95801e2
David Cournapeau <cournape@gmail.com>
2009-11-23 18:21:26 +0900

    ENH: add a parse command to toymaker for debugging purpose.

---
+++ toydist/cabal_parser/items.py
@@ -0,0 +1,5 @@
+class Flag(object):
+    def __init__(self, name, default_value, description=None):
+        self.name = name
+        self.default_value = default_value
+        self.description = description
