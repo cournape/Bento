0d2ca964dde13a2362c74c17e9fbc79721a08c32
David Cournapeau <cournape@gmail.com>
2009-11-24 16:43:52 +0900

    ENH: add a function for perl-like  substitution.

---
+++ tests/test_utils.py
@@ -0,0 +1,53 @@
+import os
+import unittest
+import tempfile
+import shutil
+
+from os.path import \
+    join
+from nose.tools import \
+    assert_equal
+
+from toydist.utils import \
+    validate_glob_pattern, expand_glob, subst_vars
+
+class TestParseGlob(unittest.TestCase):
+    def test_invalid(self):
+        # Test we raise a ValueError at unwanted pattern
+        for p in ["*/a", "src.*", "dir1/dir*/foo"]:
+            self.failUnlessRaises(ValueError, lambda: validate_glob_pattern(p))
+
+    def test_simple(self):
+        # Test simple pattern matching
+        d = tempfile.mkdtemp("flopflop")
+        try:
+            open(join(d, "foo1.py"), "w").close()
+            open(join(d, "foo2.py"), "w").close()
+            f = sorted(expand_glob("*.py", d))
+            assert_equal(f, ["foo1.py", "foo2.py"])
+        finally:
+            shutil.rmtree(d)
+
+    def test_simple2(self):
+        # Test another pattern matching, with pattern including directory
+        d = tempfile.mkdtemp("flopflop")
+        try:
+            os.makedirs(join(d, "common"))
+            open(join(d, "common", "foo1.py"), "w").close()
+            open(join(d, "common", "foo2.py"), "w").close()
+            f = sorted(expand_glob(join("common", "*.py"), d))
+            assert_equal(f, [join("common", "foo1.py"), join("common", "foo2.py")])
+        finally:
+            shutil.rmtree(d)
+
+def test_subst_vars_simple():
+    d = {'prefix': '/usr/local'}
+    assert_equal(subst_vars('$prefix', d), d['prefix'])
+
+def test_subst_vars_recusrive():
+    d = {'prefix': '/usr/local',
+         'eprefix': '$prefix',
+         'datarootdir': '$prefix/share',
+         'datadir': '$datarootdir'}
+    assert_equal(subst_vars('$eprefix', d), '/usr/local')
+    assert_equal(subst_vars('$datadir', d), '/usr/local/share')
--- toydist/utils.py
+++ toydist/utils.py
@@ -35,5 +35,41 @@ def expand_glob(pattern, ref_dir=None):
     else:
         return matched
 
+def subst_vars (s, local_vars):
+    """Perform shell/Perl-style variable substitution.
+
+    Every occurrence of '$' followed by a name is considered a variable, and
+    variable is substituted by the value found in the `local_vars' dictionary.
+    Raise ValueError for any variables not found in `local_vars'.
+
+    Parameters
+    ----------
+    s: str
+        variable to substitute
+    local_vars: dict
+        dict of variables
+    """
+    def _subst (match, local_vars=local_vars):
+        var_name = match.group(1)
+        if var_name in local_vars:
+            return str(local_vars[var_name])
+        else:
+            raise ValueError("Invalid variable '$%s'" % var)
+
+    def _do_subst(v):
+        return re.sub(r'\$([a-zA-Z_][a-zA-Z_0-9]*)', _subst, v)
+
+    try:
+        ret = _do_subst(s)
+        # Brute force: we keep interpolating until the returned string is the
+        # same as the input to handle recursion
+        while not ret == s:
+            s = ret
+            ret = _do_subst(s)
+        return ret
+    except KeyError, var:
+        raise ValueError("invalid variable '$%s'" % var)
+
+
 if __name__ == "__main__":
     print expand_glob("*.py", dirname(__file__))
