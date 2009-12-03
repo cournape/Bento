253e5a7c4510fea77b36feef388b34f7fe02ba06
David Cournapeau <cournape@gmail.com>
2009-12-03 17:46:11 +0900

    ENH: put toymaker main script in a toymakerlib, and use setuptools console_script feature to create toymaker.

--- toydist/__init__.py
+++ toydist/__init__.py
@@ -2,3 +2,6 @@ from toydist.package import \
         parse_static, static_representation
 from toydist.conv import \
         distutils_to_package_description
+
+from toydist.__version import version as __version__
+from toydist.__version import full_version as __full_version__
