580b23d0a4526af9f26b671161a8737bf52b5f5d
David Cournapeau <cournape@gmail.com>
2009-12-06 01:09:49 +0900

    REF: smarted, Reader-based installed pkg info parser.

--- toydist/installed_package_description.py
+++ toydist/installed_package_description.py
@@ -3,6 +3,8 @@ import os
 
 from toydist.utils import \
     subst_vars
+from toydist.cabal_parser.cabal_parser import \
+    ParseError
 
 META_DELIM = "!- FILELIST"
 FIELD_DELIM = ("\t", " ")
@@ -64,86 +66,143 @@ def write_file_section(name, source, target, files):
        "files": "\n".join(["\t%s" % f for f in files])}
     return section
 
-if __name__ == "__main__":
-    files = {}
-    files["pythonfiles"] = {
-            "files": ["hello.py"],
-            "target": "$sitedir",
-            }
-    p = InstalledPkgDescription(files,
-                                {"sitedir": "/usr/lib/python26/site-packages"})
-    p.write("yo.txt")
-
-def read_meta_sections(lines):
-    sections = {}
-
-    def _read_section():
-        if lines[0].startswith(FIELD_DELIM):
-            raise ValueError("First line starts with field delimiter" % \
-                             lines[0])
-        cursec = lines.pop(0).strip()
-        if cursec == "paths":
-            fields = {}
-            sections[cursec] = {}
-            while len(lines) > 0 and lines[0].startswith(FIELD_DELIM):
-                field = lines.pop(0)
-                name, value = [i.strip() for i in field.split("=")]
-                sections[cursec][name] = value
-        else:
-            raise ValueError("unrecognized section %s" % cursec)
-
-    _read_section()
-
-    return sections
-
-def read_file_sections(lines, paths):
-    sections = {}
-    if lines[0].startswith(FIELD_DELIM):
-        raise ValueError("First line starts with field delimiter" % lines[0])
-    cursec = lines.pop(0).strip()
-    if len(lines) < 2:
-        raise ValueError("no source/target ?")
-    files = []
-    sections[cursec] = files
-
-    source_field = lines.pop(0)
-    if not source_field.startswith(FIELD_DELIM):
-        raise ValueError("no source ? %s" % source_field)
-    else:
-        source_field = source_field.strip()
-        if not source_field.startswith("source="):
-            raise ValueError("no source ? %s" % source_field)
-    source = source_field.split("=")[1].strip()
-
-    target_field = lines.pop(0)
-    if not target_field.startswith(FIELD_DELIM):
-        raise ValueError("no target ? %s" % target_field)
-    else:
-        target_field = target_field.strip()
-        if not target_field.startswith("target="):
-            raise ValueError("no target ? %s" % target_field)
-    target = target_field.split("=")[1].strip()
-
-    target = subst_vars(target, paths)
-    source = subst_vars(source, paths)
-    #print source, target
-
-    while len(lines) > 0 and lines[0].startswith("\t"):
-        file = lines.pop(0).strip()
-        files.append((os.path.join(source, file), os.path.join(target, file)))
-
-    return sections
-
 def read_installed_pkg_description(filename):
     f = open(filename)
     try:
-        line = f.readline()
         meta = []
-        while not line.startswith(META_DELIM):
-            meta.append(line)
-            line = f.readline()
-        meta_sections = read_meta_sections(meta)
-        filelist = f.readlines()
-        return read_file_sections(filelist, meta_sections["paths"])
+        r = Reader(f.readlines())
+        while r.wait_for("!- FILELIST\n"):
+            meta.append(r.pop())
+        if r.eof():
+            r.parse_error("Missing filelist section")
+        r.pop()
+
+        if not meta[0].strip() == "paths":
+            raise ValueError("no path ?")
+
+        path_vars = {}
+        for i in meta[1:]:
+            name, value = [j.strip() for j in i.split("=")]
+            path_vars[name] = value
+
+        def read_section():
+            r.flush_empty()
+            line = r.peek()
+            if line and line[0] in FIELD_DELIM:
+                r.parse_error("No section found ?")
+            line = r.pop()
+            section_name = line.strip()
+
+            source = r.pop().strip()
+            target = r.pop().strip()
+            assert source.startswith("source=")
+            assert target.startswith("target=")
+            source = source.split("=")[1]
+            target = target.split("=")[1]
+
+            source = subst_vars(source, path_vars)
+            target = subst_vars(target, path_vars)
+
+            files = []
+            line = r.peek()
+            while line and line[0] in FIELD_DELIM:
+                file = r.pop().strip()
+                files.append((os.path.join(source, file), os.path.join(target, file)))
+                line = r.peek()
+
+            return section_name, files
+
+        file_sections = {}
+        while not r.eof():
+            name, files = read_section()
+            file_sections[name] = files
+        return file_sections
     finally:
         f.close()
+
+# XXX: abstract this with the reader in cabal_parser
+class Reader(object):
+    def __init__(self, data):
+        self._data = data
+        self._idx = 0
+
+    def flush_empty(self):
+        """Read until a non-empty line is found."""
+        while not (self.eof() or self._data[self._idx].strip()):
+            self._idx += 1
+
+    def pop(self, blank=False):
+        """Return the next non-empty line and increment the line
+        counter.  If `blank` is True, then also return blank lines.
+
+        """
+        if not blank:
+            # Skip to the next non-empty line if blank is not set
+            self.flush_empty()
+
+        line = self.peek(blank)
+        self._idx += 1
+
+        return line
+
+    def peek(self, blank=False):
+        """Return the next non-empty line without incrementing the
+        line counter.  If `blank` is True, also return blank lines.
+
+        Peek is not allowed to touch _idx.
+
+        """
+        if self.eof():
+            return ''
+
+        save_idx = self._idx
+        if not blank:
+            self.flush_empty()
+
+        peek_line = self._data[self._idx]
+        self._idx = save_idx
+
+        return peek_line
+
+    def eof(self):
+        """Return True if the end of the file has been reached."""
+        return self._idx >= len(self._data)
+
+    @property
+    def index(self):
+        """Return the line-counter to the pre-processed version of
+        the input file.
+
+        """
+        return self._idx
+
+    @property
+    def line(self):
+        """Return the line-counter to the original input file.
+
+        """
+        lines = 0
+        for l in self._data[:self._idx]:
+            if not l in ['{', '}']:
+                lines += 1
+        return lines
+
+    def wait_for(self, line):
+        """Keep reading until the given line has been seen."""
+        if self.eof():
+            return False
+        elif self.peek() != line:
+            return True
+        else:
+            return False
+
+    def parse_error(self, msg):
+        """Raise a parsing error with the given message."""
+        raise ParseError('''
+
+Parsing error at line %s (%s):
+%s
+Parser traceback: %s''' %
+                         (self.line, msg, self._original_data[self.line],
+                          ' -> '.join(self._traceback)))
+
