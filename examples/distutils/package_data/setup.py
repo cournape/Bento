c72760b7894d6d8ea030b6b2ac4647b241622f85
David Cournapeau <cournape@gmail.com>
2009-12-08 12:21:40 +0900

    TST: make a simpler example (without package_dir) for package data and data_files in distutils.

--- examples/distutils/package_data/setup.py
+++ examples/distutils/package_data/setup.py
@@ -6,19 +6,16 @@ from distutils.command.install import \
 class my_install(install):
     def finalize_options(self):
         install.finalize_options(self)
-        print self.config_vars
-        print dir(self)
 
 if __name__ == '__main__':
-    setup(
+    dist = setup(
         name='example',
         version='1.0',
         url='http://www.example.com',
         author='John Doe',
         author_email='john@doe.com',
         packages=['foo'],
-        package_dir={'foo': 'src/foo'},
-        package_data={'foo': ['../../data/*.dat']},
-        data_files=[('$base', ['cfg/data.cfg'])],
+        package_data={'foo': ['data/*.dat']},
+        data_files=[('etc', ['cfg/data.cfg'])],
         cmdclass={'install': my_install},
         )
