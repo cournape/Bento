779147c4761118a000475c633b6139c4aef78470
David Cournapeau <cournape@gmail.com>
2010-04-07 08:46:00 +0900

    FEAT: add win32 script creation (console only).

--- toydist/_config.py
+++ toydist/_config.py
@@ -1,7 +1,10 @@
 import os
 
 DATA_PATH = os.path.dirname(__file__)
+ARCH_DATA = os.path.dirname(__file__)
 
 _PICKLED_PARSETAB = os.path.join(DATA_PATH, "parsetab")
 _OPTIMIZE_LEX = 0
 _DEBUG_YACC = 0
+
+_CLI = os.path.join(ARCH_DATA, "commands", "cli.exe")
