698eaac3b80e4692f135cc0752a5fbeeef4ae87c
David Cournapeau <cournape@gmail.com>
2010-04-07 23:33:29 +0900

    BUG: fix setup.py to install wininst exe and ply-generated files.

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
+import shutil
 import os
 import subprocess
 import re
 
+from os.path \
+    import \
+        join, basename, dirname
+from glob \
+    import \
+        glob
+
 import setuptools
-from distutils.core import setup
+from setuptools.command.install \
+    import \
+        install as old_install
+from distutils.core \
+    import \
+        setup
 
 VERSION = "0.0.2"
 RELEASED = False
@@ -37,7 +50,45 @@ git_revision = '%s'
         f.write(cnt)
     finally:
         f.close()
-    
+
+def create_ply_tabfile():
+    from toydist.core.parser.parser import parse
+    parse('')
+
+class install(old_install):
+    def initialize_options(self):
+        old_install.initialize_options(self)
+        self.__files = []
+
+    def _copy(self, source, target):
+        if not os.path.exists(dirname(target)):
+            os.makedirs(dirname(target))
+        shutil.copy(source, target)
+        self.__files.append(target)
+
+    def run(self):
+        create_ply_tabfile()
+
+        self._copy(join("toydist", "parsetab"),
+                   join(self.install_purelib, "toydist", "parsetab"))
+
+        tdir = join(self.install_platlib, "toydist", "commands", "wininst")
+        for source in glob(join("toydist", "commands", "wininst", "*.exe")):
+            target = join(tdir, basename(source))
+            self._copy(source, target)
+
+        source = join("toydist", "commands", "cli.exe")
+        target = join(self.install_platlib, "toydist", "commands", "cli.exe")
+        self._copy(source, target)
+
+        # MUST be run after __files contains everything
+        old_install.run(self)
+
+    def get_outputs(self):
+        outfiles = old_install.get_outputs(self)
+        outfiles.extend(self.__files)
+        return outfiles
+
 DESCR = """\
 Toydist is a toy distribution tool for python packages, The goal are
 extensibility, flexibility, and easy interoperation with external tools.
@@ -76,7 +127,8 @@ PACKAGE_DATA = {
                  'toymakerlib', 'toydist.core.platforms', 'toydist.core.parser'],
     'entry_points': {
         'console_scripts': ['toymaker=toymakerlib.toymaker:noexc_main']
-    }
+    },
+    'cmdclass': {"install": install}
 }
 
 if __name__ == '__main__':
