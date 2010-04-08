51dbb7fb18f76dd3758468677dd6c191963ab0bc
David Cournapeau <cournape@gmail.com>
2010-04-08 20:20:23 +0900

    REL: add bootstrap script.

---
+++ bootstrap.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+from toydist.core \
+    import \
+        PackageDescription
+from toydist.core.utils \
+    import \
+        pprint
+
+from toydist.commands.script_utils \
+    import \
+        create_posix_script, create_win32_script
+
+def install_inplace(pkg):
+    """Install scripts of pkg in the current directory."""
+    for name, executable in pkg.executables.items():
+        if sys.platform == "win32":
+            section = create_win32_script(name, executable, ".")
+        else:
+            section = create_posix_script(name, executable, ".")
+            for f in section.files:
+                os.chmod(f, 755)
+        installed = ",".join(section.files)
+        pprint("GREEN", "installing %s in current directory" % installed)
+
+if __name__ == "__main__":
+    pkg = PackageDescription.from_file("toysetup.info")
+    if pkg.executables:
+        install_inplace(pkg)
