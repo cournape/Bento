46191aab8ec9c5850c17774e6327eb9da07732da
David Cournapeau <cournape@gmail.com>
2010-05-05 22:46:39 +0900

    REF: put extension building into separate module.

---
+++ pyext.py
@@ -0,0 +1,50 @@
+from cPickle \
+    import \
+        load, dump, dumps
+
+from ctasks \
+    import \
+        link_task
+from task_manager \
+    import \
+        get_bld, create_tasks, topo_sort, build_dag, run_tasks, CACHE_FILE
+
+def create_pyext(name, sources):
+    base = name.split(".")[-1]
+
+    tasks = []
+
+    bld = get_bld()
+
+    tasks = create_tasks(bld, sources)
+    tasks.append(link_task(bld, name.split(".")[-1]))
+
+    tuid_to_task = dict([(t.get_uid(), t) for t in tasks])
+
+    task_deps, output_to_tuid = build_dag(tasks)
+
+    yo = topo_sort(task_deps)
+    ordered_tasks = []
+    for output in yo:
+        if output in output_to_tuid:
+            ordered_tasks.append(tuid_to_task[output_to_tuid[output]])
+
+    for t in tasks:
+        t.env = bld.env
+
+    run_tasks(bld, ordered_tasks)
+
+    with open(CACHE_FILE, "w") as fid:
+        dump(bld.cache, fid)
+
+def create_sources(sources):
+    bld = get_bld()
+
+    tasks = create_tasks(bld, sources)
+    run_tasks(bld, tasks)
+
+    with open(CACHE_FILE, "w") as fid:
+        dump(bld.cache, fid)
+if __name__ == "__main__":
+    create_sources(sources=["foo.h.in"])
+    create_pyext("_bar", ["hellomodule.c", "foo.c"])
--- task_manager.py
+++ task_manager.py
@@ -6,7 +6,8 @@ from cPickle \
 
 from task import Task, template_task
 from ctasks \
-    import ccompile_task, link_task
+    import \
+        ccompile_task
 
 RULES_REGISTRY = {
         ".c": ccompile_task,
@@ -92,34 +93,6 @@ def topo_sort(task_deps):
 
     return tmp
 
-def create_pyext(name, sources):
-    base = name.split(".")[-1]
-
-    tasks = []
-
-    bld = get_bld()
-
-    tasks = create_tasks(bld, sources)
-    tasks.append(link_task(bld, name.split(".")[-1]))
-
-    tuid_to_task = dict([(t.get_uid(), t) for t in tasks])
-
-    task_deps, output_to_tuid = build_dag(tasks)
-
-    yo = topo_sort(task_deps)
-    ordered_tasks = []
-    for output in yo:
-        if output in output_to_tuid:
-            ordered_tasks.append(tuid_to_task[output_to_tuid[output]])
-
-    for t in tasks:
-        t.env = bld.env
-
-    run_tasks(bld, ordered_tasks)
-
-    with open(CACHE_FILE, "w") as fid:
-        dump(bld.cache, fid)
-
 def get_bld():
     bld = BuildContext()
     bld.env = {"CC": ["gcc"],
@@ -136,16 +109,3 @@ def get_bld():
         bld.cache = {}
 
     return bld
-
-def create_sources(sources):
-    bld = get_bld()
-
-    tasks = create_tasks(bld, sources)
-    run_tasks(bld, tasks)
-
-    with open(CACHE_FILE, "w") as fid:
-        dump(bld.cache, fid)
-
-if __name__ == "__main__":
-    create_sources(sources=["foo.h.in"])
-    create_pyext("_bar", ["hellomodule.c", "foo.c"])
