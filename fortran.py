e6a6a63e4f92a31646d938f3faabd0b7f351dfeb
David Cournapeau <cournape@gmail.com>
2010-05-09 00:37:17 +0900

    ENH: add build dir support to fortran task.

--- fortran.py
+++ fortran.py
@@ -12,11 +12,20 @@ from task \
 from compiled_fun \
     import \
         compile_fun
+from utils \
+    import \
+        ensure_dir
 
 @extension(".f")
 def fortran_task(self, node):
     base = os.path.splitext(node)[0]
-    target = base + ".o"
+    # XXX: hack to avoid creating build/build/... when source is
+    # generated. Dealing with this most likely requires a node concept
+    if not base.startswith(self.env["BLDDIR"]):
+        target = os.path.join(self.env["BLDDIR"], base + ".o")
+    else:
+        target = base + ".o"
+    ensure_dir(target)
     task = Task("f77", inputs=node, outputs=target)
     task.env_vars = ["F77", "F77FLAGS"]
     task.env = self.env
