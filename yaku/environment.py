6579eb6cd21ea7c0fb2c61c319405c4db837e028
David Cournapeau <cournape@gmail.com>
2010-05-20 08:52:57 +0900

    FEAT: add an environment class.
    We will start using this instead of a plain environment to hold
    configuration and build variables - the class is like a dict but has
    easyily readable storage format.

---
+++ yaku/environment.py
@@ -0,0 +1,30 @@
+import re
+import os
+
+re_imp = re.compile('^(#)*?([^#=]*?)\ =\ (.*?)$', re.M)
+
+class Environment(dict):
+    def get_flat(self, k):
+        s = self[k]
+        if isinstance(s, str):
+            return s
+        else:
+            return " ".join(s)
+
+    def store(self, filename):
+        tmp = filename + ".tmp"
+        fid = open(tmp, "w")
+        for k in sorted(self.keys()):
+            fid.write("%s = %r" % (k, self[k]))
+
+        try:
+            os.unlink(filename)
+        except OSError:
+            pass
+
+        os.rename(tmp, filename)
+
+    def load(self, filename):
+        f = open(filename)
+        for m in re_imp.finditer(f.read()):
+            self[m.group(2)] = eval(m.group(3))
