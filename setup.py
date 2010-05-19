52d74ca8695ca47d9ca058e5ba439f15d2e6f874
David Cournapeau <cournape@gmail.com>
2010-05-19 21:20:07 +0900

    FEAT: add simple setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core \
+    import \
+        setup
+
+setup(name="yaku",
+    description="A simple build system in python to build python extensions",
+    long_description=open("README").read(),
+    version="0.0.1",
+    author="David Cournapeau",
+    author_email="cournape@gmail.com",
+    license="BSD",
+    packages=["yaku.tools", "yaku"],
+)
