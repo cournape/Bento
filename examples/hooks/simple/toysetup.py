a3eb44e153f821e82741975fdd9fea81ec0c83a1
David Cournapeau <cournape@gmail.com>
2010-05-25 08:53:43 +0900

    DOC: improve the simple hook example a bit.

--- examples/hooks/simple/toysetup.py
+++ examples/hooks/simple/toysetup.py
@@ -8,16 +8,19 @@ class TestCommand(Command):
     def run(self, opts):
         pprint('BLUE', "Running test command....")
 
-# Example to register new commands
-# XXX: a natural way to do this would be to mark classes which would follow
-# the command protocol, but python 2.x does not support class decorators
+# Example to register new commands: startup function is called everytime
+# toymaker is run, and very early on. This is the place where to e.g. register
+# new commands
 def startup():
     register_command("test", TestCommand)
 
-# Example to override existing toydist command:
+# Example to override existing toydist command: we add an extra source file
+# here instead of adding it in toysetup.info. This may be used to e.g. support
+# the setuptools feature of automatically adding files in VCS.
 @override
 def sdist(opts):
-    pkg = get_configured_state().pkg
-    pkg.extra_source_files.append("toysetup.py")
+    ctx = get_configured_state()
+    pkg = ctx.pkg
+    pkg.extra_source_files.append("toysetup.py.bak")
 
     create_tarball(pkg)
