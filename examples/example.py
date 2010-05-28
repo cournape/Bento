56281e7fae31ba30a98c592c08b104588bbf83d5
David Cournapeau <cournape@gmail.com>
2010-05-28 22:07:05 +0900

    ENH: adapt example.py to new context API.

--- examples/example.py
+++ examples/example.py
@@ -1,46 +1,46 @@
-import distutils
 import sys
 
 from yaku.pyext \
     import \
         create_pyext, get_pyenv
-
 from yaku.task_manager \
     import \
         create_tasks, run_tasks
-from yaku.build_context \
+from yaku.context \
     import \
-        get_bld
-from yaku.tools \
-    import \
-        import_tools
+        get_bld, get_cfg
 
-import_tools(["ctasks", "tpl_tasks", "cython", "fortran", "swig"], ["tools"])
+from yaku.tools.gcc import detect as gcc_detect
+from yaku.tools.gfortran import detect as gfortran_detect
 
-def create_sources(bld, name, sources):
-    tasks = create_tasks(bld, sources)
-    run_tasks(bld, tasks)
-
-if __name__ == "__main__":
-    from yaku.tools.gcc import detect as gcc_detect
-    from yaku.tools.gfortran import detect as gfortran_detect
-
-    bld = get_bld()
-    bld.env.update({
+def configure(ctx):
+    ctx.load_tools(["ctasks", "tpl_tasks", "cython",
+                    "fortran", "swig"], ["tools"])
+    ctx.env.update({
             #"SWIG": ["swig"],
             #"SWIGFLAGS": ["-python"],
             "SUBST_DICT": {"VERSION": "0.0.2"}})
-    if "-v" in sys.argv:
-        bld.env["VERBOSE"] = True
-    bld.env.update(get_pyenv())
+    ctx.env.update(get_pyenv())
 
-    gcc_detect(bld)
-    gfortran_detect(bld)
+    gcc_detect(ctx)
+    gfortran_detect(ctx)
 
-    create_sources(bld, "template", sources=["src/foo.h.in"])
-    create_pyext(bld, "_bar", ["src/hellomodule.c", "src/foo.c"])
-    create_pyext(bld, "_von", ["src/vonmises_cython.pyx"])
-    #create_pyext(bld, "_fortran_yo", ["src/bar.f"])
-    #create_pyext(bld, "_swig_yo", ["src/yo.i"])
+def build(ctx):
+    create_sources(ctx, "template", sources=["src/foo.h.in"])
+    create_pyext(ctx, "_bar", ["src/hellomodule.c", "src/foo.c"])
+    create_pyext(ctx, "_von", ["src/vonmises_cython.pyx"])
+    #create_pyext(ctx, "_fortran_yo", ["src/bar.f"])
+    #create_pyext(ctx, "_swig_yo", ["src/yo.i"])
 
-    bld.save()
+def create_sources(ctx, name, sources):
+    tasks = create_tasks(ctx, sources)
+    run_tasks(ctx, tasks)
+
+if __name__ == "__main__":
+    ctx = get_cfg()
+    configure(ctx)
+    ctx.store()
+
+    ctx = get_bld()
+    build(ctx)
+    ctx.store()
