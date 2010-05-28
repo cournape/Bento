0ec9bf888502ee1a2da0f26d49f637346ef30806
David Cournapeau <cournape@gmail.com>
2010-05-28 21:19:04 +0900

    ENH: load/store for configure context implemented.

--- examples/example1.py
+++ examples/example1.py
@@ -1,6 +1,10 @@
 import os
 import sys
 
+from cPickle \
+    import \
+        load, dump
+
 from yaku.pyext \
     import \
         create_pyext, get_pyenv
@@ -11,8 +15,15 @@ from yaku.build_context \
 from yaku.tools \
     import \
         import_tools
+from yaku.utils \
+    import \
+        ensure_dir
 
 from yaku.tools.gcc import detect as gcc_detect
+
+from yaku.conftests \
+    import \
+        check_compiler, check_header
 
 # Use dot in the name to avoid accidental import of it
 CONFIG_INFO = "default.config.py"
@@ -23,6 +34,8 @@ def configure(ctx):
     if "-v" in sys.argv:
         ctx.env["VERBOSE"] = True
     gcc_detect(ctx)
+    check_compiler(ctx)
+    check_header(ctx, "stdio.h")
     ctx.env.update(get_pyenv())
 
 def build(ctx):
@@ -43,6 +56,7 @@ class ConfigureContext(object):
         self.env = {}
         self.tools = []
         self.cache = {}
+        self.conf_results = []
 
     def load_tools(self, tools, tooldir=None):
         for t in tools:
@@ -52,20 +66,36 @@ class ConfigureContext(object):
     def store(self):
         self.env.store(DEFAULT_ENV)
 
+        self.log.close()
+        with open(CONFIG_CACHE, "w") as fid:
+            dump(self.cache, fid)
+
 class BuildContext(object):
     def __init__(self):
         self.env = {}
         self.tools = {}
         self.cache = {}
 
+def myopen(filename, mode="r"):
+    if "w" in mode:
+        ensure_dir(filename)
+    return open(filename, mode)
+
 def get_cfg():
     ctx = ConfigureContext()
+    if os.path.exists(CONFIG_CACHE):
+        with open(CONFIG_CACHE) as fid:
+            ctx.cache = load(fid)
+
     if os.path.exists(DEFAULT_ENV):
         env = Environment()
         env.load(DEFAULT_ENV)
     else:
         env = Environment()
+    if not env.has_key("BLDDIR"):
+        env["BLDDIR"] = BUILD_DIR
     ctx.env = env
+    ctx.log = myopen(os.path.join(env["BLDDIR"], "config.log"), "w")
     return ctx
 
 if __name__ == "__main__":
