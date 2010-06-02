ffeba88ab7d8efcd47fe6b9f95c2558279155a29
David Cournapeau <cournape@gmail.com>
2010-06-02 14:03:14 +0900

    ENH+DOC: yaku-based build now works + one example.

--- bento/commands/build_yaku.py
+++ bento/commands/build_yaku.py
@@ -1,2 +1,31 @@
+import sys
+import os
+
+from bento.installed_package_description \
+    import \
+        InstalledSection
+
+import yaku.context
+
 def build_extensions(pkg):
-    return {}
+    bld = yaku.context.get_bld()
+    if "-v" in sys.argv:
+        bld.env["VERBOSE"] = True
+
+    ret = {}
+    for ext in pkg.extensions.values():
+        outputs = bld.builders["pyext"].extension(ext.name, ext.sources)
+
+        # FIXME: do package -> location translation correctly
+        pkg_dir = os.path.dirname(ext.name.replace('.', os.path.sep))
+        target = os.path.join('$sitedir', pkg_dir)
+        fullname = ext.name
+        ext_targets = outputs
+        # FIXME: assume all outputs of one extension are in one directory
+        srcdir = os.path.dirname(ext_targets[0])
+        section = InstalledSection("extensions", fullname, srcdir,
+                                    target, [os.path.basename(o) for o in outputs])
+        ret[fullname] = section
+    bld.store()
+
+    return ret
