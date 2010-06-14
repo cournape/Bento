c2cdaaa3de9fb716009245a5a695c2fb33b267c5
David Cournapeau <cournape@gmail.com>
2010-06-14 09:06:38 +0900

    REF: set up gcc one per one.

--- yaku/tools/gcc.py
+++ yaku/tools/gcc.py
@@ -1,15 +1,16 @@
 def detect(ctx):
     env = ctx.env
 
-    ctx.env.update(
-       {"CC": ["gcc"],
-        "CFLAGS": ["-Wall"],
-        "LINK": ["gcc"],
-        "LINKFLAGS": [],
-        "SHLINK": ["gcc", "-shared"],
-        "SHLINKFLAGS": [],
-        "CPPPATH": [],
-        "LIBDIR": [],
-        "LIBS": [],
-        "LIBS_FMT": "-l%s",
-        "LIBDIR_FMT": "-L%s"})
+    ctx.env["CC"] = ["gcc"]
+    ctx.env["CC_TGT_F"] = ["-c", "-o"]
+    ctx.env["CC_TGT_G"] = ["-c", "-o"]
+    ctx.env["CFLAGS"] = ["-Wall"]
+    ctx.env["LINK"] = ["gcc"]
+    ctx.env["LINKFLAGS"] = []
+    ctx.env["SHLINK"] = ["gcc", "-shared"]
+    ctx.env["SHLINKFLAGS"] = []
+    ctx.env["CPPPATH"] = []
+    ctx.env["LIBDIR"] = []
+    ctx.env["LIBS"] = []
+    ctx.env["LIB_FMT"] = "-l%s"
+    ctx.env["LIBDIR_FMT"] = "-L%s"
