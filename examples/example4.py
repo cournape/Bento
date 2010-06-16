bc085c4f4f5800a219e460065accdce57e4753f3
David Cournapeau <cournape@gmail.com>
2010-06-16 09:11:13 +0900

    DOC: add simple executable example.

---
+++ examples/example4.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from yaku.context \
+    import \
+        get_bld, get_cfg
+from yaku.scheduler \
+    import \
+        run_tasks
+
+def configure(ctx):
+    tools = ctx.use_tools(["ctasks"])
+    if sys.platform == "win32":
+        import yaku.tools.msvc as msvc
+        msvc.detect(ctx)
+    else:
+        import yaku.tools.gcc as gcc
+        gcc.detect(ctx)
+
+def build(ctx):
+    builder = ctx.builders["ctasks"]
+    builder.program("main", ["src/main.c"])
+
+if __name__ == "__main__":
+    ctx = get_cfg()
+    configure(ctx)
+    ctx.store()
+
+    ctx = get_bld()
+    build(ctx)
+    run_tasks(ctx)
+    ctx.store()
