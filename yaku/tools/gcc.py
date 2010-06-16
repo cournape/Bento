bb0c3f9f7583e3f3c11ca6f3ba8000eb9e5c1da8
David Cournapeau <cournape@gmail.com>
2010-06-16 09:09:00 +0900

    ENH: add a few flags in gcc, fix typo.

--- yaku/tools/gcc.py
+++ yaku/tools/gcc.py
@@ -3,13 +3,16 @@ def detect(ctx):
 
     ctx.env["CC"] = ["gcc"]
     ctx.env["CC_TGT_F"] = ["-c", "-o"]
-    ctx.env["CC_TGT_G"] = ["-c", "-o"]
+    ctx.env["CC_SRC_F"] = []
     ctx.env["CFLAGS"] = ["-Wall"]
     ctx.env["LINK"] = ["gcc"]
     ctx.env["LINKFLAGS"] = []
+    ctx.env["LINK_TGT_F"] = ["-o"]
+    ctx.env["LINK_SRC_F"] = []
     ctx.env["SHLINK"] = ["gcc", "-shared"]
     ctx.env["SHLINKFLAGS"] = []
     ctx.env["CPPPATH"] = []
+    ctx.env["CPPPATH_FMT"] = "-I%s"
     ctx.env["LIBDIR"] = []
     ctx.env["LIBS"] = []
     ctx.env["LIB_FMT"] = "-l%s"
