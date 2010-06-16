37fab5f626ab7c37a56a712735dbcef2a25c1da1
David Cournapeau <cournape@gmail.com>
2010-06-17 00:56:02 +0900

    ENH: handle msvc special conventions when building the cmd line.

--- yaku/tools/msvc.py
+++ yaku/tools/msvc.py
@@ -1,3 +1,5 @@
+import yaku.task
+
 def detect(ctx):
     env = ctx.env
 
@@ -13,3 +15,21 @@ def detect(ctx):
     ctx.env["LIB_FMT"] = "%s.lib"
     ctx.env["LIBDIR"] = []
     ctx.env["LIBDIR_FMT"] = "/L%s"
+
+    ctx.env["PROGRAM_FMT"] = "%s.exe"
+    ctx.env["CC_OBJECT_FMT"] = "%s.obj"
+
+    # XXX: hack
+    saved = yaku.task.Task.exec_command
+    def msvc_exec_command(self, cmd, cwd):
+        new_cmd = []
+        carry = ""
+        for c in cmd:
+            if c in ["/Fo", "/out:"]:
+                carry = c
+            else:
+                c = carry + c
+                carry = ""
+                new_cmd.append(c)
+        saved(self, new_cmd, cwd)
+    yaku.task.Task.exec_command = msvc_exec_command
