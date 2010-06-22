2de9bdffcf15e7c0e6c95bc0822eb4cc9c202751
David Cournapeau <cournape@gmail.com>
2010-06-23 08:45:18 +0900

    DOC: include main page into gh script.

--- update_gh.sh
+++ update_gh.sh
@@ -24,9 +24,12 @@ python setup.py install
 
 (cd doc && make html)
 mv doc/build/html $TEMPDIR
+mv doc/main_page/index.* $TEMPDIR
+mv doc/main_page/chunkfive.css $TEMPDIR
+mv doc/main_page/reset.css $TEMPDIR
 mv $TEMPDIR/html/_static $TEMPDIR/html/static
 find $TEMPDIR/html -type f -exec perl -p -i -e s/_static/static/g '{}' \;
 git checkout gh-pages || exit
 rm -rf $GH_PAGES_REPO/*
-mv $TEMPDIR/html $GH_PAGES_REPO
+mv $TEMPDIR/* $GH_PAGES_REPO/
 rm -rf $TEMPDIR
