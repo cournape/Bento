a7163c6a9469ea98d614b3487cb37f8db07c30ba
David Cournapeau <cournape@gmail.com>
2010-07-31 17:47:09 +0900

    FEAT: add recurse decorator to recurse hook files.

--- bento/commands/hooks.py
+++ bento/commands/hooks.py
@@ -1,3 +1,11 @@
+import os
+import sys
+import imp
+
+from bento.compat \
+    import \
+        inspect as compat_inspect
+
 from bento.commands.core \
     import \
         register_command
@@ -18,7 +26,10 @@ def add_to_registry(func, category):
 def override_command(command, func):
     global __COMMANDS_OVERRIDE
 
-    __COMMANDS_OVERRIDE[command] = (func,)
+    if __COMMANDS_OVERRIDE.has_key(command):
+        __COMMANDS_OVERRIDE[command].append(func)
+    else:
+        __COMMANDS_OVERRIDE[command] = [func]
 
 def add_to_pre_registry(func, cmd_name):
     global __PRE_HOOK_REGISTRY
@@ -89,3 +100,29 @@ def command_register(f, *a, **kw):
     ret = f(*a, **kw)
     for cmd_name, cmd_class in ret.items():
         register_command(cmd_name, cmd_class)
+
+# XXX: consolidate this with the similar code in bentomakerlib
+def create_hook_module(target):
+    par_bscript = compat_inspect.stack()[2][1]
+    target = os.path.join(os.path.dirname(par_bscript), target)
+    if not os.path.exists(target):
+        raise ValueError("Recurse target file %s not found" % target)
+
+    safe_name = target.replace("/", "_")
+    module_name = "bento_hook_%s" % safe_name
+    module = imp.new_module(module_name)
+    module.__file__ = os.path.abspath(target)
+    code = open(target).read()
+
+    sys.path.insert(0, os.path.dirname(target))
+    try:
+        exec(compile(code, target, 'exec'), module.__dict__)
+        sys.modules[module_name] = module
+    finally:
+        sys.path.pop(0)
+
+def recurse(targets):
+    def _f(a):
+        for target in targets:
+            create_hook_module(target)
+    return _f
