63286631016091770c0ec8317a7e9f8f4e67d0af
David Cournapeau <cournape@gmail.com>
2010-08-03 09:21:41 +0900

    ENH: make gfortran tool a bit more like gcc.

--- yaku/tools/gfortran.py
+++ yaku/tools/gfortran.py
@@ -1,6 +1,16 @@
-def detect(ctx):
+import yaku.utils
+
+def setup(ctx):
     env = ctx.env
 
     ctx.env.update(
        {"F77": ["gfortran"],
-        "F77FLAGS": ["-W", "-g"]})
+        "F77FLAGS": ["-W", "-g"],
+        "F77_TGT_F": ["-o"],
+        "F77_SRC_F": ["-c"]})
+
+def detect(ctx):
+    if yaku.utils.find_program("gcc") is None:
+        return False
+    else:
+        return True
