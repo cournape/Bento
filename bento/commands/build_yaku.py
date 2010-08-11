af6d80bbe27dbd1559be65fd0b1562ec3aa884c5
David Cournapeau <cournape@gmail.com>
2010-08-11 21:34:20 +0900

    REF: refactor common code between clib and pyext build.

--- bento/commands/build_yaku.py
+++ bento/commands/build_yaku.py
@@ -17,6 +17,23 @@ import yaku.context
 import yaku.scheduler
 import yaku.errors
 
+def run_tasks(bld, all_outputs, inplace):
+    task_manager = yaku.task_manager.TaskManager(bld.tasks)
+    runner = yaku.scheduler.SerialRunner(bld, task_manager)
+    runner.start()
+    runner.run()
+
+    if inplace:
+        # FIXME: do package -> location + remove hardcoded extension
+        # FIXME: handle in-place at yaku level
+        for ext, outputs in all_outputs.items():
+            for o in outputs:
+                target = os.path.join(
+                            os.path.dirname(ext.replace(".", os.sep)),
+                            os.path.basename(o.abspath()))
+                shutil.copy(o.abspath(), target)
+    return
+
 def build_isection(bld, ext_name, files):
     # Given an extension name and the list of files which constitute
     # it (e.g. the .so on unix, the .pyd on windows, etc...), return
@@ -54,68 +71,34 @@ def _build_extensions(bld, pkg, inplace, verbose):
         all_outputs[ext.name] = outputs
         ret[ext.name] = build_isection(bld, ext.name, outputs)
 
-    task_manager = yaku.task_manager.TaskManager(bld.tasks)
-    runner = yaku.scheduler.SerialRunner(bld, task_manager)
-    runner.start()
-    runner.run()
-
-    if inplace:
-        so_ext = bld.builders["pyext"].env["PYEXT_SO"]
-        # FIXME: do package -> location + remove hardcoded extension
-        # FIXME: handle in-place at yaku level
-        for ext, outputs in all_outputs.items():
-            for o in outputs:
-                target = os.path.join(
-                            os.path.dirname(ext.replace(".", os.sep)),
-                            os.path.basename(o.abspath()))
-                shutil.copy(o.abspath(), target)
+    run_tasks(bld, all_outputs, inplace)
     return ret
 
-def build_compiled_library(bld, pkg, inplace, verbose):
+def build_compiled_library(bld, extension, verbose):
+    builder = bld.builders["ctasks"]
+    try:
+        if verbose:
+            builder.env["VERBOSE"] = True
+        for p in extension.include_dirs:
+            builder.env["CPPPATH"].insert(0, p)
+        return builder.static_library(extension.name,
+                                      extension.sources)
+    except RuntimeError, e:
+        msg = "Building extension %s failed: %s" % (extension.name, str(e))
+        raise CommandExecutionFailure(msg)
+
+def _build_compiled_libraries(bld, pkg, inplace, verbose):
     ret = {}
     if len(pkg.compiled_libraries) < 1:
         return  ret
 
     all_outputs = {}
     for ext in pkg.compiled_libraries.values():
-        builder = bld.builders["ctasks"]
-        try:
-            if verbose:
-                builder.env["VERBOSE"] = True
-            for p in ext.include_dirs:
-                builder.env["CPPPATH"].insert(0, p)
-            outputs = builder.static_library(ext.name, ext.sources)
-            all_outputs[ext.name] = outputs
-        except RuntimeError, e:
-            msg = "Building extension %s failed: %s" % (ext.name, str(e))
-            raise CommandExecutionFailure(msg)
+        outputs = build_compiled_library(bld, ext, verbose)
+        all_outputs[ext.name] = outputs
+        ret[ext.name] = build_isection(bld, ext.name, outputs)
 
-        # FIXME: do package -> location translation correctly
-        pkg_dir = os.path.dirname(ext.name.replace('.', os.path.sep))
-        target = os.path.join('$sitedir', pkg_dir)
-        fullname = ext.name
-        ext_targets = outputs
-        # FIXME: assume all outputs of one extension are in one directory
-        srcdir = os.path.dirname(ext_targets[0])
-        section = InstalledSection("extensions", fullname, srcdir,
-                                    target, [os.path.basename(o) for o in outputs])
-        ret[fullname] = section
-
-    task_manager = yaku.task_manager.TaskManager(bld.tasks)
-    runner = yaku.scheduler.SerialRunner(bld, task_manager)
-    runner.start()
-    runner.run()
-
-    if inplace:
-        so_ext = bld.builders["pyext"].env["PYEXT_SO"]
-        # FIXME: do package -> location + remove hardcoded extension
-        # FIXME: handle in-place at yaku level
-        for ext, outputs in all_outputs.items():
-            for o in outputs:
-                target = os.path.join(
-                            os.path.dirname(ext.replace(".", os.sep)),
-                            os.path.basename(o))
-                shutil.copy(o, target)
+    run_tasks(bld, all_outputs, inplace)
     return ret
 
 import bento.core.errors
@@ -135,7 +118,7 @@ def build_extensions(ctx, pkg, inplace=False, verbose=False):
 def build_compiled_libraries(ctx, pkg, inplace=False, verbose=False):
     bld = ctx.yaku_build_ctx
     try:
-        return build_compiled_library(bld, pkg, inplace, verbose)
+        return _build_compiled_libraries(bld, pkg, inplace, verbose)
     except yaku.errors.TaskRunFailure, e:
         if e.explain:
             msg = e.explain
