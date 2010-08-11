aad8147f7ca4901e5d22623e89092485307f4c41
David Cournapeau <cournape@gmail.com>
2010-08-11 20:52:04 +0900

    REF: factor out per-extension builder.

--- bento/commands/build_yaku.py
+++ bento/commands/build_yaku.py
@@ -17,33 +17,41 @@ import yaku.context
 import yaku.scheduler
 import yaku.errors
 
-def build_extension(bld, pkg, inplace, verbose):
+def build_isection(bld, ext_name, files):
+    # Given an extension name and the list of files which constitute
+    # it (e.g. the .so on unix, the .pyd on windows, etc...), return
+    # an InstallSection
+
+    # FIXME: do package -> location translation correctly
+    pkg_dir = os.path.dirname(ext_name.replace('.', os.path.sep))
+    target = os.path.join('$sitedir', pkg_dir)
+
+    # FIXME: assume all outputs of one extension are in one directory
+    srcdir = files[0].parent.path_from(bld.bld_root)
+    section = InstalledSection("extensions", ext_name, srcdir,
+                                target, [o.name for o in files])
+    return section
+
+def build_extension(bld, extension, verbose):
+    builder = bld.builders["pyext"]
+    try:
+        if verbose:
+            builder.env["VERBOSE"] = True
+        return builder.extension(extension.name, extension.sources)
+    except RuntimeError, e:
+        msg = "Building extension %s failed: %s" % \
+              (extension.name, str(e))
+        raise CommandExecutionFailure(msg)
+
+def _build_extensions(bld, pkg, inplace, verbose):
     ret = {}
     if len(pkg.extensions) < 1:
         return  ret
 
     all_outputs = {}
     for ext in pkg.extensions.values():
-        builder = bld.builders["pyext"]
-        try:
-            if verbose:
-                builder.env["VERBOSE"] = True
-            outputs = builder.extension(ext.name, ext.sources)
-            all_outputs[ext.name] = outputs
-        except RuntimeError, e:
-            msg = "Building extension %s failed: %s" % (ext.name, str(e))
-            raise CommandExecutionFailure(msg)
-
-        # FIXME: do package -> location translation correctly
-        pkg_dir = os.path.dirname(ext.name.replace('.', os.path.sep))
-        target = os.path.join('$sitedir', pkg_dir)
-        fullname = ext.name
-        ext_targets = outputs
-        # FIXME: assume all outputs of one extension are in one directory
-        srcdir = ext_targets[0].parent.path_from(bld.bld_root)
-        section = InstalledSection("extensions", fullname, srcdir,
-                                    target, [o.name for o in outputs])
-        ret[fullname] = section
+        outputs = build_extension(bld, ext, verbose)
+        ret[ext.name] = build_isection(bld, ext.name, outputs)
 
     task_manager = yaku.task_manager.TaskManager(bld.tasks)
     runner = yaku.scheduler.SerialRunner(bld, task_manager)
@@ -114,7 +122,7 @@ import bento.core.errors
 def build_extensions(ctx, pkg, inplace=False, verbose=False):
     bld = ctx.yaku_build_ctx
     try:
-        return build_extension(bld, pkg, inplace, verbose)
+        return _build_extensions(bld, pkg, inplace, verbose)
     except yaku.errors.TaskRunFailure, e:
         if e.explain:
             msg = e.explain
