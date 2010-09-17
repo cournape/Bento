fab04fd88feda08e96af9ce76234150391ccb244
David Cournapeau <cournape@gmail.com>
2010-09-17 23:13:19 +0900

    FEAT: add pauli 2to3 utils, and plug it into setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
+import os
+import sys
+import shutil
+
 from distutils.core \
     import \
         setup
+
+if sys.version_info >= (3,):
+    local_path = os.path.dirname(__file__)
+    src_path = os.path.join(local_path, 'build', 'py3k')
+    sys.path.insert(0, os.path.join(local_path, 'tools'))
+    import py3tool
+    print("Converting to Python3 via 2to3...")
+    py3tool.sync_2to3('yaku', os.path.join(src_path, 'yaku'))
 
 setup(name="yaku",
     description="A simple build system in python to build python extensions",
