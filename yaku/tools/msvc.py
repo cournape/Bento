3f16b3d5b6dd565454d9e51d98279d3e74509a02
David Cournapeau <cournape@gmail.com>
2010-10-12 09:06:46 +0900

    ENH: add /nologo to msvc to avoid annoying banners.

--- yaku/tools/msvc.py
+++ yaku/tools/msvc.py
@@ -6,7 +6,7 @@ def setup(ctx):
     ctx.env["CC"] = ["cl.exe"]
     ctx.env["CC_TGT_F"] = ["/c", "/Fo"]
     ctx.env["CC_SRC_F"] = []
-    ctx.env["CFLAGS"] = []
+    ctx.env["CFLAGS"] = ["/nologo"]
     ctx.env["CPPPATH"] = []
     ctx.env["CPPPATH_FMT"] = "/I%s"
     ctx.env["LINK"] = ["link.exe"]
