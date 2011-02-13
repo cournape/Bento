02fbc850255572a6d7a7f2d9fc8cc8fdd8708479
David Cournapeau <cournape@gmail.com>
2011-02-14 08:52:08 +0900

    REF: start a commands.api module.

---
+++ bento/commands/api.py
@@ -0,0 +1,44 @@
+from bento.commands.core \
+    import \
+        HelpCommand
+from bento.commands.configure \
+    import \
+        ConfigureCommand
+from bento.commands.build \
+    import \
+        BuildCommand
+from bento.commands.install \
+    import \
+        InstallCommand
+from bento.commands.parse \
+    import \
+        ParseCommand
+from bento.commands.convert \
+    import \
+        ConvertCommand
+from bento.commands.sdist \
+    import \
+        SdistCommand
+from bento.commands.detect_type \
+    import \
+        DetectTypeCommand
+from bento.commands.build_pkg_info \
+    import \
+        BuildPkgInfoCommand
+from bento.commands.build_egg \
+    import \
+        BuildEggCommand
+from bento.commands.build_wininst \
+    import \
+        BuildWininstCommand
+from bento.commands.distcheck \
+    import \
+        DistCheckCommand
+
+from bento.commands.core \
+    import \
+        COMMANDS_REGISTRY
+
+from bento.commands.errors \
+    import \
+        ConvertionError, UsageException, CommandExecutionFailure
