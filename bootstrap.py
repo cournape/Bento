6c3f46c6b33c0bda743bac9147550b7603ee7842
David Cournapeau <cournape@gmail.com>
2011-02-24 11:52:06 +0900

    ENH: create python-specific versions of bentomaker.

--- bootstrap.py
+++ bootstrap.py
@@ -17,17 +17,23 @@ from bento.commands.script_utils \
 MODE_755 = stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP | \
     stat.S_IROTH | stat.S_IXOTH
 
+def _create_executable(name, executable):
+    if sys.platform == "win32":
+        section = create_win32_script(name, executable, ".")
+    else:
+        section = create_posix_script(name, executable, ".")
+        for f, g in section.files:
+            os.chmod(g, MODE_755)
+    return section
+
 def install_inplace(pkg):
     """Install scripts of pkg in the current directory."""
-    for name, executable in pkg.executables.items():
-        if sys.platform == "win32":
-            section = create_win32_script(name, executable, ".")
-        else:
-            section = create_posix_script(name, executable, ".")
-            for f, g in section.files:
-                os.chmod(g, MODE_755)
-        installed = ",".join([g for f, g in section.files])
-        pprint("GREEN", "installing %s in current directory" % installed)
+    for basename, executable in pkg.executables.items():
+        version_str = ".".join([str(i) for i in sys.version_info[:2]])
+        for name in [basename, "%s-%s" % (basename, version_str)]:
+            section = _create_executable(name, executable)
+            installed = ",".join([g for f, g in section.files])
+            pprint("GREEN", "installing %s in current directory" % installed)
 
 if __name__ == "__main__":
     from setup_common import generate_version_py
