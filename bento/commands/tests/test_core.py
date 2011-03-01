bb6ec85bd1c3a7c770b5574481beb19d2c0502be
David Cournapeau <cournape@gmail.com>
2011-03-01 19:34:53 +0900

    TST: add some dummy tests to cover bento.commands

---
+++ bento/commands/tests/test_core.py
@@ -0,0 +1,12 @@
+from bento.commands.configure \
+    import \
+        ConfigureCommand
+from bento.commands.build \
+    import \
+        BuildCommand
+from bento.commands.install \
+    import \
+        InstallCommand
+from bento.commands.sdist \
+    import \
+        SdistCommand
---
+++ bento/commands/tests/test_misc.py
@@ -0,0 +1,6 @@
+from bento.commands.build_wininst \
+    import \
+        BuildWininstCommand
+from bento.commands.build_egg \
+    import \
+        BuildEggCommand
