baae295cf55fb676e99bf02f3cba869664dc48a8
David Cournapeau <cournape@gmail.com>
2011-03-08 22:21:06 +0900

    DOC: add link to waf example.

--- examples/simples/single_extension_waf/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-from bento.core import PackageDescription
-from bento.conv import pkg_to_distutils
-
-pkg = PackageDescription.from_file("bento.info")
-setup(**pkg_to_distutils(pkg))
