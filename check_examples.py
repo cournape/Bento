45194c09efa0fcd704ad78efc435e56ac0c402bf
David Cournapeau <cournape@gmail.com>
2011-03-08 09:08:31 +0900

    TST: count errors when running examples + test caching.

--- check_examples.py
+++ check_examples.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import shutil
 
 tests = []
 for root, d, files in os.walk("examples"):
@@ -8,19 +9,31 @@ for root, d, files in os.walk("examples"):
             tests.append(root)
 
 def test_package(d):
-    for bcmd in [["configure"], ["build"], ["install", "--list-files"]]:
-        cmd = [bentomaker] + bcmd
-        p = subprocess.Popen(cmd, cwd=test, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        p.wait()
-        if p.returncode:
-            print p.stdout.read()
-            return False
-    return True
+    def _run():
+        for bcmd in [["configure"], ["build"], ["install", "--list-files"]]:
+            cmd = [bentomaker] + bcmd
+            p = subprocess.Popen(cmd, cwd=test, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            p.wait()
+            if p.returncode:
+                print p.stdout.read()
+                return False
+        return True
+    if os.path.exists(os.path.join(d, "build")):
+        shutil.rmtree(os.path.join(d, "build"))
+    if _run():
+        # We run twice to check that re-running commands with various bento caching
+        # does not blow up
+        return _run()
+    else:
+        return False
 
+nerrors = 0
 bentomaker = os.path.join(os.getcwd(), "bentomaker")
 for test in tests:
     print "=============== testing %s ==============" % test
     if not test_package(test):
         print "Failed"
+        nerrors += 1
     else:
         print "Succeeded"
+print "%d / %d example failed" % (nerrors, len(tests))
