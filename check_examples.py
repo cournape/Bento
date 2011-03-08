51ad40830a89fb538662361d95f509274eaab2b2
David Cournapeau <cournape@gmail.com>
2011-03-08 09:02:53 +0900

    TST: test install for examples as well.

--- check_examples.py
+++ check_examples.py
@@ -8,7 +8,7 @@ for root, d, files in os.walk("examples"):
             tests.append(root)
 
 def test_package(d):
-    for bcmd in [["configure"], ["build"]]:
+    for bcmd in [["configure"], ["build"], ["install", "--list-files"]]:
         cmd = [bentomaker] + bcmd
         p = subprocess.Popen(cmd, cwd=test, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         p.wait()
