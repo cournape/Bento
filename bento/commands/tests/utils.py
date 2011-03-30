7b9599fd6091e5a2c83856f8fff401ec2a99d7c8
David Cournapeau <cournape@gmail.com>
2011-03-30 21:07:18 +0900

    TST: add a function to create a package fileset from its bento.info.

--- bento/commands/tests/utils.py
+++ bento/commands/tests/utils.py
@@ -1,3 +1,5 @@
+import os
+
 from bento._config \
     import \
         BUILD_DIR
@@ -13,6 +15,30 @@ from bento.commands.options \
 from bento.commands.context \
     import \
         ConfigureYakuContext
+
+DUMMY_C = r"""\
+#include <Python.h>
+#include <stdio.h>
+
+static PyObject*
+hello(PyObject *self, PyObject *args)
+{
+    printf("Hello from C\n");
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyMethodDef HelloMethods[] = {
+    {"hello",  hello, METH_VARARGS, "Print a hello world."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC
+init%(name)s(void)
+{
+    (void) Py_InitModule("%(name)s", HelloMethods);
+}
+"""
 
 def prepare_configure(top_node, bento_info, context_klass=ConfigureYakuContext):
     # FIXME: this should be created in the configure context
@@ -33,4 +59,45 @@ def prepare_configure(top_node, bento_info, context_klass=ConfigureYakuContext):
     context.package_options = package_options
 
     return context, configure
+
+def create_fake_package_from_bento_info(top_node, bento_info):
+    from bento.core.package import raw_parse, raw_to_pkg_kw
+    d = raw_parse(bento_info)
+    _kw, files = raw_to_pkg_kw(d, {}, None)
+    kw = {}
+    if "extensions" in _kw:
+        kw["extensions"] = _kw["extensions"].values()
+    if "py_modules" in _kw:
+        kw["modules"] = _kw["py_modules"]
+    if "packages" in _kw:
+        kw["packages"] = _kw["packages"]
+    return create_fake_package(top_node, **kw)
+
+def create_fake_package(top_node, packages=None, modules=None, extensions=[]):
+    if packages is None:
+        packages = []
+    if modules is None:
+        modules = []
+    if extensions is None:
+        extensions = []
+
+    for p in packages:
+        d = p.replace(".", os.sep)
+        n = top_node.make_node(d)
+        n.mkdir()
+        init = n.make_node("__init__.py")
+        init.write("")
+    for m in modules:
+        d = m.replace(".", os.sep)
+        n = top_node.make_node("%s.py" % d)
+        n.parent.mkdir()
+        n.write("")
+    for extension in extensions:
+        main = extension.sources[0]
+        n = top_node.make_node(main)
+        n.parent.mkdir()
+        n.write(DUMMY_C % {"name": extension.name})
+        for s in extension.sources[1:]:
+            n = top_node.make_node(s)
+            n.write("")
 
