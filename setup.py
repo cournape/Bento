c573337d085067fb66bf10e6aa5d0632c4b87bed
David Cournapeau <cournape@gmail.com>
2011-04-09 13:09:17 +0900

    BUG: monkey-patch setuptools in setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # setuptools should be first so that bento.distutils knows what to monkey patch
 import setuptools
 import bento.distutils
+bento.distutils.monkey_patch()
 
 from setuptools \
     import \
