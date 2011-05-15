6785308b50316f6c1b8e3f4d64e87ed6436b9677
David Cournapeau <cournape@gmail.com>
2011-05-15 18:31:35 +0900

    REF: put installed sections registration in build context.

--- bento/commands/build.py
+++ bento/commands/build.py
@@ -33,6 +33,15 @@ from bento.commands.script_utils \
     import \
         create_posix_script, create_win32_script
 
+class SectionWriter(object):
+    def __init__(self):
+        self.sections = {}
+
+    def store(self, filename, pkg):
+        meta = ipkg_meta_from_pkg(pkg)
+        p = InstalledPkgDescription(self.sections, meta, pkg.executables)
+        p.write(filename)
+
 class BuildCommand(Command):
     long_descr = """\
 Purpose: build the project
@@ -60,10 +69,6 @@ Usage:   bentomaker build [OPTIONS]."""
             p.print_help()
             return
 
-        self.section_writer.sections["pythonfiles"] = build_python_files(ctx)
-        self.section_writer.sections["datafiles"] = build_data_files(ctx)
-        self.section_writer.sections["executables"] = build_executables(ctx)
-
         ctx.compile()
         ctx.post_compile(self.section_writer)
 
@@ -71,44 +76,13 @@ Usage:   bentomaker build [OPTIONS]."""
         n = ctx.build_node.make_node(IPKG_PATH)
         self.section_writer.store(n.abspath(), ctx.pkg)
 
-def build_python_files(ctx):
-    py_sections = {}
-    def build_py_isection(name, nodes, from_node=None):
-        if from_node is None:
-            from_node = ctx.top_node
-        source_dir = from_node.bldpath()
-        isection = InstalledSection.from_source_target_directories("pythonfiles",
-            name, os.path.join("$_srcrootdir", source_dir), "$sitedir",
-            [n.path_from(from_node) for n in nodes])
-        py_sections[name] = isection
-    for name, nodes in ctx._node_pkg.iter_category("packages"):
-        build_py_isection(name, nodes)
-    for name, node in ctx._node_pkg.iter_category("modules"):
-        build_py_isection(name, [node])
-    if ctx.pkg.config_py:
-        content = _config_content(ctx.get_paths_scheme())
-        target_node = ctx.build_node.make_node(ctx.pkg.config_py)
-        target_node.parent.mkdir()
-        target_node.safe_write(content)
-        build_py_isection("bento_config", [target_node], ctx.build_node)
-    return py_sections
-
-def build_data_files(ctx):
-    data_sections = {}
-    for name, section in ctx._node_pkg.iter_category("datafiles"):
-        source_dir = os.path.join("$_srcrootdir", section.ref_node.bldpath())
-        data_sections[name] = InstalledSection.from_source_target_directories("datafiles", name,
-            source_dir, section.target_dir, [n.path_from(section.ref_node) for n in section.nodes])
-    return data_sections
-
-def build_executables(ctx):
-    scripts_node = ctx.build_node.make_node("scripts-%s" % sys.version[:3])
-    scripts_node.mkdir()
-    executable_sections = {}
-    for name, executable in ctx.pkg.executables.iteritems():
-        section = build_executable(name, executable, scripts_node)
-        executable_sections[name] = section
-    return executable_sections
+def build_py_isection(context, name, nodes, from_node=None):
+    if from_node is None:
+        from_node = context.top_node
+    source_dir = from_node.bldpath()
+    return InstalledSection.from_source_target_directories("pythonfiles",
+        name, os.path.join("$_srcrootdir", source_dir), "$sitedir",
+        [n.path_from(from_node) for n in nodes])
 
 def build_isection(bld, ext_name, files, category):
     """Build an InstalledSection from the list of files for an
@@ -137,15 +111,6 @@ def build_isection(bld, ext_name, files, category):
         os.path.join("$_srcrootdir", source_dir.bldpath()),
         target_dir, [o.path_from(source_dir) for o in nodes])
     return section
-
-class SectionWriter(object):
-    def __init__(self):
-        self.sections = {}
-
-    def store(self, filename, pkg):
-        meta = ipkg_meta_from_pkg(pkg)
-        p = InstalledPkgDescription(self.sections, meta, pkg.executables)
-        p.write(filename)
 
 def _config_content(paths):
     keys = sorted(paths.keys())
