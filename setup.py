364f1a088ec5dcddc3d51afe979fa1682dc0c882
David Cournapeau <cournape@gmail.com>
2011-06-28 08:31:26 +0900

    STY: shorten setup.py.

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ import setuptools
 import bento.distutils
 bento.distutils.monkey_patch()
 
-from setuptools \
-    import \
-        setup
-
 from setup_common \
     import \
         generate_version_py
 
 if __name__ == '__main__':
     generate_version_py("bento/__dev_version.py")
-    setup()
+    setuptools.setup()
