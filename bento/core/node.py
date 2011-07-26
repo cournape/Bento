3696a4d0fdf81f7c7d8bcb428207cd2d9db2688b
David Cournapeau <cournape@gmail.com>
2011-07-26 12:28:14 +0900

    BUG: fix invalid exception.

--- bento/core/node.py
+++ bento/core/node.py
@@ -450,7 +450,7 @@ class Node(object):
                             #print "pattern", k
                             accu.append(re.compile(k))
                         except Exception, e:
-                            raise Errors.WafError("Invalid pattern: %s" % k, e)
+                            raise ValueError("Invalid pattern: %s" % k, e)
                 ret.append(accu)
             return ret
 
