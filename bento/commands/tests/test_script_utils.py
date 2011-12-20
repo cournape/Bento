cefec4beea3387eb3ddb1cc17e898c79401d39c7
David Cournapeau <cournape@gmail.com>
2011-12-20 22:41:18 +0000

    TST: add simple test for nt_quote_args.

---
+++ bento/commands/tests/test_script_utils.py
@@ -0,0 +1,16 @@
+import unittest
+
+from bento.commands.script_utils \
+    import \
+        nt_quote_arg
+
+class TestMisc(unittest.TestCase):
+    def test_nt_quote_arg(self):
+        s = nt_quote_arg("foo")
+        self.assertEqual(s, "foo")
+
+        s = nt_quote_arg(" foo")
+        self.assertEqual(s, '" foo"')
+
+        s = nt_quote_arg("\ foo")
+        self.assertEqual(s, '"\\ foo"')
