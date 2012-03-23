2bec7031fa2a8ed5fd92bd2badbefa1c902a1c93
David Cournapeau <cournape@gmail.com>
2012-03-23 11:16:22 +0000

    BUG: fix broken test.

--- bento/convert/tests/test_convert.py
+++ bento/convert/tests/test_convert.py
@@ -1,20 +1,26 @@
+import os
 import sys
 
 from bento.misc.testing \
     import \
         SubprocessTestCase
-
+from bento.core.node \
+    import \
+        create_first_node
 from bento.convert.core \
     import \
         monkey_patch
 
 class TestMonkeyPatch(SubprocessTestCase):
+    def setUp(self):
+        self.top_node = create_first_node(os.getcwd())
+
     def test_distutils(self):
-        monkey_patch("distutils", "setup.py")
+        monkey_patch(self.top_node, "distutils", "setup.py")
         self.assertTrue("setuptools" not in sys.modules)
 
     def test_setuptools(self):
-        monkey_patch("setuptools", "setup.py")
+        monkey_patch(self.top_node, "setuptools", "setup.py")
         self.assertTrue("setuptools" in sys.modules)
 
 #class TestMonkeyPatch(SubprocessTestCase):
