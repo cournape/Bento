16a60748d0d67962d12274687e9ad8ffe294dc93
David Cournapeau <cournape@gmail.com>
2012-03-29 16:32:27 +0100

    REF: remove old, complicated that can be simply replaced by node.

--- bento/core/tests/test_subpackage.py
+++ bento/core/tests/test_subpackage.py
@@ -1,36 +1,21 @@
-import os
 import shutil
 import tempfile
 
 from bento.compat.api.moves \
     import \
         unittest
-
-import bento.core.node as node
+from bento.core.node \
+    import \
+        create_first_node, find_root
 import bento.core.pkg_objects as pkg_objects
 import bento.core.package as package
 import bento.core.subpackage as subpackage
 
-def create_file(file, makedirs=True):
-    if makedirs:
-        dirname = os.path.dirname(file)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-    fid = open(file, "w").close()
-
-def create_fake_tree(top, files):
-    for f in files:
-        create_file(os.path.join(top, f))
-
-def clean_tree(files):
-    dirs = []
-    for f in files:
-        dirs.append(os.path.dirname(f))
-        if os.path.exists(f):
-            os.remove(f)
-
-    for d in sorted(set(dirs))[::-1]:
-        os.rmdir(d)
+def create_fake_tree(top_node, tree):
+    for f in tree:
+        n = top_node.make_node(f)
+        n.parent.mkdir()
+        n.write("")
 
 class TestTranslation(unittest.TestCase):
     """Those tests check 'translation' from local definition (as
@@ -47,19 +32,18 @@ class TestTranslation(unittest.TestCase):
                         rdir="foo",
                         extensions={"_hello": extension})
 
-        top = tempfile.mkdtemp()
+        d = tempfile.mkdtemp()
         try:
+            top = create_first_node(d)
             create_fake_tree(top, tree)
-            root = node.Node("", None)
-            top_node = root.find_dir(top)
             extensions = subpackage.flatten_subpackage_extensions(
-                    spkg, top_node)
+                    spkg, top)
             self.failUnless("foo._hello" in extensions)
             extension = extensions["foo._hello"]
             self.assertEqual(extension.sources, ["foo/src/hellomodule.c"])
             self.assertEqual(extension.include_dirs, ["foo"])
         finally:
-            shutil.rmtree(top)
+            shutil.rmtree(d)
 
     def test_compiled_library(self):
         tree = [
@@ -73,16 +57,15 @@ class TestTranslation(unittest.TestCase):
                         rdir="bar",
                         compiled_libraries={"clib": clib})
 
-        top = tempfile.mkdtemp()
+        d = tempfile.mkdtemp()
         try:
+            top = create_first_node(d)
             create_fake_tree(top, tree)
-            root = node.Node("", None)
-            top_node = root.find_dir(top)
             clibs = subpackage.flatten_subpackage_compiled_libraries(
-                    spkg, top_node)
+                    spkg, top)
             self.failUnless("bar.clib" in clibs)
             clib = clibs["bar.clib"]
             self.assertEqual(clib.sources, ["bar/src/clib.c"])
             self.assertEqual(clib.include_dirs, ["bar"])
         finally:
-            shutil.rmtree(top)
+            shutil.rmtree(d)
