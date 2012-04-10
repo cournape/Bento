54c6e9c74cff9630f895865444e2e57f8636df86
David Cournapeau <cournape@gmail.com>
2012-04-10 11:55:01 +0100

    ENH: cover both bento and bentomakerlib packages.

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,1 +1,1 @@
-python2.7 -m nose.core --with-coverage --cover-package=bento --cover-html bento
+python2.7 -m nose.core --with-coverage --cover-package=bento,bentomakerlib --cover-html bento bentomakerlib
