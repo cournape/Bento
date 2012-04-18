748b43f20d19b3c14be24957e26b5974e1a84a57
David Cournapeau <cournape@gmail.com>
2012-04-18 08:59:49 +0100

    FEAT: add utilities to register package to pypi.
    Most of the code is taken from distutils2 with a few minor modifications
    + tests.

---
+++ bento/commands/register_utils.py
@@ -0,0 +1,241 @@
+import urllib2
+
+import os.path as op
+
+from six.moves \
+    import \
+        configparser, StringIO
+
+from bento.conv \
+    import \
+        pkg_to_distutils_meta
+from bento.core.utils \
+    import \
+        extract_exception
+
+DEFAULT_REPOSITORY = 'http://pypi.python.org/pypi'
+DEFAULT_REALM = 'pypi'
+
+REALM = DEFAULT_REALM
+REPOSITORY = 'http://testpypi.python.org/pypi'
+
+_BOUNDARY = '--------------GHSKFJDLGDS7543FJKLFHRE75642756743254'
+
+def _read_new_format(config, repository):
+    sections = config.sections()
+
+    # let's get the list of servers
+    index_servers = config.get('distutils', 'index-servers')
+    _servers = [server.strip() for server in
+                index_servers.split('\n')
+                if server.strip() != '']
+    if _servers == []:
+        # nothing set, let's try to get the default pypi
+        if 'pypi' in sections:
+            _servers = ['pypi']
+        else:
+            # the file is not properly defined
+            return PyPIConfig()
+    for server in _servers:
+        current = PyPIConfig()
+        current.username = config.get(server, 'username')
+        current.server = server
+
+        # optional params
+        def _get_default(key, default):
+            if config.has_option(server, key):
+                return config.get(server, key)
+            else:
+                return default
+
+        current.repository = _get_default("repository", DEFAULT_REPOSITORY)
+        current.realm = _get_default("realm", DEFAULT_REALM)
+        current.password = _get_default("password", None)
+
+        if (current.server == repository or current.repository == repository):
+            return current
+    return PyPIConfig()
+
+def _read_old_format(config):
+    server = 'server-login'
+    if config.has_option(server, 'repository'):
+        repository = config.get(server, 'repository')
+    else:
+        repository = DEFAULT_REPOSITORY
+
+    return PyPIConfig(username=config.get(server, 'username'),
+            password=config.get(server, 'password'),
+            repository=repository,
+            server=server,
+            realm=DEFAULT_REALM)
+
+def read_pypirc(repository=DEFAULT_REPOSITORY):
+    """Read the default .pypirc file.
+
+    Returns a PyPIConfig instance if the default .pypirc can be found. Raises
+    an IOError otherwise
+
+    Parameters
+    ----------
+    repository: str
+        repository to use
+    """
+    rc = op.join(op.expanduser('~'), '.pypirc')
+    if op.exists(rc):
+        fp = open(rc, "rt")
+        try:
+            return parse_pypirc(fp, repository)
+        finally:
+            fp.close()
+    else:
+        return IOError("Default pypirc config file not found: %r" % rc)
+
+def parse_pypirc(fp, repository=DEFAULT_REPOSITORY):
+    """Parse the given pypi config file.
+
+    Returns a PyPIConfig instance if the file can be parsed.
+
+    Parameters
+    ----------
+    fp: file-like object
+        contains the content of the config file
+    repository: str
+        repository to look for
+    """
+    config = configparser.RawConfigParser()
+    config.readfp(fp)
+    sections = config.sections()
+    if 'distutils' in sections:
+        return _read_new_format(config, repository)
+    elif 'server-login' in sections:
+        return _read_old_format(config)
+    else:
+        msg = "Unrecognized format"
+        if hasattr(fp, "name"):
+            msg += " (for file %r)" % fp.name
+        raise ValueError(msg)
+
+class PyPIConfig(object):
+    @classmethod
+    def from_file(cls, fp=None, repository=DEFAULT_REPOSITORY):
+        """Create a PyPIConfig instance from the give file for the give repository.
+
+        Parameters
+        ----------
+        fp: file-like object or None
+            If None, attemps to read the .pypirc file. Otherwise, must be a
+            file-like object
+        repository: str
+            Repository to consider in the .pypirc file.
+        """
+        if fp is None:
+            return read_pypirc(repository)
+        else:
+            return parse_pypirc(fp, repository)
+
+    @classmethod
+    def from_string(cls, s, repository=DEFAULT_REPOSITORY):
+        return cls.from_file(StringIO(s), repository)
+
+    def __init__(self, username=None, password=None, repository=None,
+            server=None, realm=None):
+        self.username = username
+        self.password = password
+        self.repository = repository
+        self.realm = realm
+        self.server = server
+
+def encode_multipart(fields, files, boundary=None):
+    """Prepare a multipart HTTP request.
+
+    *fields* is a sequence of (name: str, value: str) elements for regular
+    form fields, *files* is a sequence of (name: str, filename: str, value:
+    bytes) elements for data to be uploaded as files.
+
+    Returns (content_type: bytes, body: bytes) ready for httplib.HTTP.
+    """
+    # Taken from http://code.activestate.com/recipes/146306
+
+    if boundary is None:
+        boundary = _BOUNDARY
+    elif not isinstance(boundary, str):
+        raise TypeError('boundary must be str, not %r' % type(boundary))
+
+    l = []
+    for key, values in fields:
+        # handle multiple entries for the same name
+        if not isinstance(values, (tuple, list)):
+            values = [values]
+
+        for value in values:
+            l.extend((
+                '--' + boundary,
+                # XXX should encode to match packaging but it causes bugs
+                ('Content-Disposition: form-data; name="%s"' % key),
+                '',
+                value))
+
+    for key, filename, value in files:
+        l.extend((
+            '--' + boundary,
+            ('Content-Disposition: form-data; name="%s"; filename="%s"' %
+             (key, filename)),
+            '',
+            value))
+
+    l.append('--' + boundary + '--')
+    l.append('')
+
+    body = '\r\n'.join(l)
+    content_type = 'multipart/form-data; boundary=' + boundary
+    return content_type, body
+
+def build_post_data(pkg, action):
+    data = pkg_to_distutils_meta(pkg)
+    data[":action"] = action
+    return data
+
+def post_to_server(post_data, config, auth=None):
+    """Send the given post_data to the pypi server.
+
+    Parameters
+    ----------
+    post_data: dict
+        Usually the dict returned by build_post_data
+    config: object
+        A PyPIConfig instance
+    auth: object or None
+        HTTP authentification object.
+
+    Returns
+    -------
+    code: int
+        HTTP status code
+    msg: str
+        Message received back from the server
+    """
+    content_type, body = encode_multipart(post_data.items(), [])
+
+    # build the Request
+    headers = {
+        'Content-type': content_type,
+        'Content-length': str(len(body))
+    }
+    req = urllib2.Request(config.repository, body, headers)
+
+    # handle HTTP and include the Basic Auth handler
+    opener = urllib2.build_opener(
+        urllib2.HTTPBasicAuthHandler(password_mgr=auth)
+    )
+    try:
+        opener.open(req)
+    except urllib2.HTTPError:
+        e = extract_exception()
+        code, msg = e.code, e.msg
+    except urllib2.URLError:
+        e = extract_exception()
+        code, msg = 500, str(e)
+    else:
+        code, msg = 200, 'OK'
+
+    return code, msg
---
+++ bento/commands/tests/test_register_utils.py
@@ -0,0 +1,196 @@
+import shutil
+import tempfile
+import urllib2
+import urlparse
+
+import os.path as op
+
+from six.moves \
+    import \
+        StringIO
+
+import mock
+
+from bento.commands.register_utils \
+    import \
+        build_post_data, encode_multipart, post_to_server, DEFAULT_REALM, \
+        DEFAULT_REPOSITORY, PyPIConfig, parse_pypirc, parse_pypirc
+from bento.compat.api.moves \
+    import \
+        unittest
+from bento.core \
+    import \
+        PackageDescription
+
+class TestRegisterUtils(unittest.TestCase):
+    def test_build_post_data(self):
+        r_content = """----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="maintainer"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="name"\r\n\r\n""" \
+"""foo\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="license"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="author"\r\n\r\n""" \
+"""John Doe\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="url"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name=":action"\r\n\r\n""" \
+"""submit\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="download_url"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="maintainer_email"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="author_email"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="version"\r\n\r\n""" \
+"""1.0\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="long_description"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254\r\n""" \
+"""Content-Disposition: form-data; name="description"\r\n\r\n\r\n""" \
+"""----------------GHSKFJDLGDS7543FJKLFHRE75642756743254--\r\n""" \
+""""""
+        bento_info = """\
+Name: foo
+Version: 1.0
+Author: John Doe
+"""
+        package = PackageDescription.from_string(bento_info)
+        post_data = build_post_data(package, "submit")
+        content_type, body = encode_multipart(post_data.items(), [])
+        self.assertEqual(r_content, body)
+
+    @mock.patch("urllib2.OpenerDirector", mock.MagicMock())
+    def test_register_server(self):
+        package = PackageDescription(name="foo")
+        repository = 'http://testpypi.python.org/pypi'
+        realm = DEFAULT_REALM
+        config = PyPIConfig(username="cdavid", password="yoyo", repository=repository, realm=realm)
+
+        auth = urllib2.HTTPPasswordMgr()
+        host = urlparse.urlparse(config.repository)[0]
+        auth.add_password(config.realm, host, config.username, config.password)
+
+        post_data = build_post_data(package, "submit")
+        code, msg = post_to_server(post_data, config, auth)
+        self.assertEqual(code, 200)
+        self.assertEqual(msg, "OK")
+
+    @mock.patch("urllib2.OpenerDirector.open",
+                mock.MagicMock(side_effect=urllib2.HTTPError("", 404, "", {}, None)))
+    def test_register_server_http_errors(self):
+        code, msg = self._test_register_server_errors()
+        self.assertEqual(code, 404)
+        self.assertEqual(msg, "")
+
+    @mock.patch("urllib2.OpenerDirector.open",
+                mock.MagicMock(side_effect=urllib2.URLError("")))
+    def test_register_server_url_errors(self):
+        code, msg = self._test_register_server_errors()
+        self.assertEqual(code, 500)
+
+    def _test_register_server_errors(self):
+        package = PackageDescription(name="foo")
+        config = PyPIConfig.from_string("""
+[distutils]
+index-servers = pypi
+
+[pypi]
+username = cdavid
+password = yoyo
+server = http://testpypi.python.org
+""")
+
+        post_data = build_post_data(package, "submit")
+        return post_to_server(post_data, config)
+
+class TestReadPyPI(unittest.TestCase):
+    def test_pypi_config(self):
+        data = """\
+[distutils]
+index-servers =
+    test
+
+[test]
+username:cdavid
+password:yoyo
+repository:http://testpypi.python.org
+"""
+        config = PyPIConfig.from_file(StringIO(data), repository="test")
+        self.assertEqual(config.username, "cdavid")
+        self.assertEqual(config.password, "yoyo")
+
+    def test_new_format_default(self):
+        data = """\
+[distutils]
+index-servers =
+
+[pypi]
+username:cdavid
+password:yoyo
+repository:http://testpypi.python.org
+"""
+        config = parse_pypirc(StringIO(data), 'pypi')
+        self.assertEqual(config.username, "cdavid")
+        self.assertEqual(config.password, "yoyo")
+        self.assertEqual(config.repository, "http://testpypi.python.org")
+
+    def test_invalid_format(self):
+        self.assertRaises(ValueError, parse_pypirc, StringIO(""))
+
+    def test_new_format(self):
+        data = """\
+[distutils]
+index-servers =
+    test
+
+[test]
+username:cdavid
+password:yoyo
+repository:http://testpypi.python.org
+"""
+        config = parse_pypirc(StringIO(data))
+        self.assertEqual(config.username, None)
+        self.assertEqual(config.password, None)
+
+        config = parse_pypirc(StringIO(data), repository="test")
+        self.assertEqual(config.username, "cdavid")
+        self.assertEqual(config.password, "yoyo")
+        self.assertEqual(config.repository, "http://testpypi.python.org")
+
+    def test_old_format(self):
+        data = """\
+[server-login]
+username:cdavid
+password:yoyo
+repository:http://testpypi.python.org
+"""
+        config = parse_pypirc(StringIO(data))
+        self.assertEqual(config.username, "cdavid")
+        self.assertEqual(config.password, "yoyo")
+        self.assertEqual(config.repository, "http://testpypi.python.org")
+
+class TestReadPyPIDefault(unittest.TestCase):
+    def setUp(self):
+        self.d = tempfile.mkdtemp()
+        try:
+            self.p = mock.patch("os.path.expanduser", lambda x: self.d)
+            self.p.start()
+        except:
+            shutil.rmtree(self.d)
+            raise
+
+    def tearDown(self):
+        self.p.stop()
+        shutil.rmtree(self.d)
+
+    def test_read_pypirc(self):
+        fp = open(op.join(op.expanduser("~"), ".pypirc"), "wt")
+        try:
+            fp.write("")
+        finally:
+            fp.close()
