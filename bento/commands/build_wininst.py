c6a5f1fcf2bbec33b3cfd1b4a7846cfd5a92f35b
David Cournapeau <cournape@gmail.com>
2012-04-27 22:41:18 +0100

    FEAT: add output-dir/file options to build_wininst.

--- bento/commands/build_wininst.py
+++ bento/commands/build_wininst.py
@@ -30,6 +30,11 @@ class BuildWininstCommand(Command):
 Purpose: build wininst
 Usage:   bentomaker build_wininst [OPTIONS]"""
     short_descr = "build wininst."
+    common_options = Command.common_options \
+                        + [Option("--output-dir",
+                                  help="Output directory", default="dist"),
+                           Option("--output-file",
+                                  help="Output filename")]
 
     def run(self, ctx):
         argv = ctx.command_argv
@@ -41,16 +46,20 @@ Usage:   bentomaker build_wininst [OPTIONS]"""
 
         n = ctx.build_node.make_node(IPKG_PATH)
         ipkg = InstalledPkgDescription.from_file(n.abspath())
-        create_wininst(ipkg, src_root_node=ctx.build_node, build_node=ctx.build_node)
+        create_wininst(ipkg, src_root_node=ctx.build_node, build_node=ctx.build_node,
+                       wininst=o.output_file,
+                       output_dir=o.output_dir)
 
-def create_wininst(ipkg, src_root_node, build_node, egg_info=None, wininst=None):
+def create_wininst(ipkg, src_root_node, build_node, egg_info=None, wininst=None, output_dir=None):
     meta = PackageMetadata.from_ipkg(ipkg)
     if egg_info is None:
         egg_info = EggInfo.from_ipkg(ipkg, build_node)
 
     # XXX: do this correctly, maybe use same as distutils ?
     if wininst is None:
-        wininst = wininst_filename(os.path.join("dist", meta.fullname))
+        wininst = wininst_filename(os.path.join(output_dir, meta.fullname))
+    else:
+        wininst = os.path.join(output_dir, wininst)
     ensure_dir(wininst)
 
     egg_info_dir = os.path.join("PURELIB", egg_info_dirname(meta.fullname))
