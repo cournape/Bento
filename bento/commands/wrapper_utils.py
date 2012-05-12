2aee30d4200d1c3cd3777e6edc2e31a420d35f4e
David Cournapeau <cournape@gmail.com>
2012-05-12 20:13:14 +0100

    BUG: fix mismatch between dependent and main command argv.

--- bento/commands/wrapper_utils.py
+++ bento/commands/wrapper_utils.py
@@ -13,8 +13,8 @@ def run_with_dependencies(global_context, cmd_name, cmd_argv, run_node, top_node
     global_context."""
     deps = global_context.retrieve_dependencies(cmd_name)
     for dep_cmd_name in deps:
-        cmd_argv = global_context.retrieve_command_argv(cmd_name)
-        resolve_and_run_command(global_context, dep_cmd_name, cmd_argv, run_node, top_node, package)
+        dep_cmd_argv = global_context.retrieve_command_argv(cmd_name)
+        resolve_and_run_command(global_context, dep_cmd_name, dep_cmd_argv, run_node, top_node, package)
     resolve_and_run_command(global_context, cmd_name, cmd_argv, run_node, top_node, package)
 
 def resolve_and_run_command(global_context, cmd_name, cmd_argv, run_node, top_node, package):
