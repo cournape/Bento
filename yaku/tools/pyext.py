c3afe51ebb3df9f9cf8a9f97d89c4a8444d6eb0c
David Cournapeau <cournape@gmail.com>
2012-06-09 11:17:53 +0800

    BUG: temporary workaround in pyext to get clang working.

--- yaku/tools/pyext.py
+++ yaku/tools/pyext.py
@@ -468,9 +468,9 @@ def _setup_compiler(ctx, cc_type):
         sys.path.pop(0)
         ctx.env = old_env
 
-    copied_values = ["CPPPATH_FMT", "LIBDIR_FMT", "LIB_FMT",
+    copied_values = ["CC", "CPPPATH_FMT", "LIBDIR_FMT", "LIB_FMT",
             "CC_OBJECT_FMT", "CC_TGT_F", "CC_SRC_F", "LINK_TGT_F",
-            "LINK_SRC_F"]
+            "LINK_SRC_F", "SHLINK"]
     for k in copied_values:
         ctx.env["PYEXT_%s" % k] = cc_env[k]
     ctx.env.prextend("PYEXT_CPPPATH", cc_env["CPPPATH"])
