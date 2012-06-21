4106dd43ff0bdcb033c1f07baf71027a20794bb7
David Cournapeau <cournape@gmail.com>
2012-06-21 23:20:37 +0100

    REF: remove useless code.

--- bento/distutils/commands/build.py
+++ bento/distutils/commands/build.py
@@ -4,15 +4,6 @@ from distutils.command.build \
 
 class build(old_build):
     cmd_name = "build"
-    def __init__(self, *a, **kw):
-        old_build.__init__(self, *a, **kw)
-
-    def initialize_options(self):
-        old_build.initialize_options(self)
-
-    def finalize_options(self):
-        old_build.finalize_options(self)
-
     def run(self):
         self.run_command("config")
         self.distribution.run_command_in_context(self.cmd_name, [])
--- bento/distutils/commands/sdist.py
+++ bento/distutils/commands/sdist.py
@@ -5,15 +5,6 @@ from distutils.command.sdist \
         sdist as old_sdist
 
 class sdist(old_sdist):
-    def __init__(self, *a, **kw):
-        old_sdist.__init__(self, *a, **kw)
-
-    def initialize_options(self):
-        old_sdist.initialize_options(self)
-
-    def finalize_options(self):
-        old_sdist.finalize_options(self)
-
     def run(self):
         cmd_name = "sdist"
         cmd_argv = ["--output-dir=%s" % self.dist_dir]
