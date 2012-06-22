dbd8cc23e8d8d02f2153ea83921aafcbb14e7450
David Cournapeau <cournape@gmail.com>
2012-06-22 23:49:54 +0100

    REF: extract msi tree building into separate function.

--- bento/commands/build_msi.py
+++ bento/commands/build_msi.py
@@ -16,6 +16,17 @@ from bento.private.bytecode \
     import \
         bcompile, PyCompileError
 
+def build_msi_tree(build_manifest, src_root_node, msi_tree_root):
+    msi_scheme = {"prefix": msi_tree_root.abspath(),
+                  "eprefix": msi_tree_root.abspath()}
+
+    for kind, source, target in build_manifest.iter_built_files(src_root_node, msi_scheme):
+        if kind == "pythonfiles":
+            compiled = target.change_ext(".pyc")
+            compiled.safe_write(bcompile(source.abspath()))
+        target.parent.mkdir()
+        shutil.copy(source.abspath(), target.abspath())
+
 class BuildMsiCommand(Command):
     long_descr = """\
 Purpose: build MSI
@@ -36,19 +47,9 @@ Usage:   bentomaker build_msi [OPTIONS]"""
         output_file = o.output_file
 
         n = context.build_node.find_node(IPKG_PATH)
-        ipackage = BuildManifest.from_file(n.abspath())
-
+        manifest = BuildManifest.from_file(n.abspath())
         msi_root = context.build_node.make_node("msi")
-        src_root_node = context.build_node
 
-        ipackage.update_paths({"prefix": msi_root.abspath(), "eprefix": msi_root.abspath()})
-        file_sections = ipackage.resolve_paths(src_root_node)
-
-        for kind, source, target in iter_files(file_sections):
-            if kind == "pythonfiles":
-                compiled = target.change_ext(".pyc")
-                compiled.safe_write(bcompile(source.abspath()))
-            target.parent.mkdir()
-            shutil.copy(source.abspath(), target.abspath())
+        build_msi_tree(manifest, context.build_node, msi_root)
 
         create_msi_installer(context.pkg, context.run_node, msi_root, o.output_file, o.output_dir)
