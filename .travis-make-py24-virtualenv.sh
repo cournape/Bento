bd182cdbed7d0a198fc0e2d7b8957734e8a0ed92
David Cournapeau <cournape@gmail.com>
2012-09-26 22:37:59 +0100

    TST: attempt to make 2.4 work on travis-ci.

--- .travis-make-py24-virtualenv.sh
+++ .travis-make-py24-virtualenv.sh
@@ -5,6 +5,7 @@ VIRTENV=$1
 set -x
 set -e
 
+curl -O http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.7.2.tar.gz
 curl -O http://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2
 tar xjf Python-2.4.6.tar.bz2
 cd Python-2.4.6
@@ -15,4 +16,7 @@ EOF
 ./configure --prefix=$PWD/install
 make
 make install
-virtualenv -p install/bin/python2.4 --distribute $VIRTENV
+tar xzf virtualenv-1.7.2.tar.gz
+(cd virtualenv-1.7.2 && ../install/bin/python2.4 setup.py install)
+#virtualenv -p install/bin/python2.4 --distribute $VIRTENV
+./install/bin/virtualenv -p install/bin/python2.4 --distribute $VIRTENV
